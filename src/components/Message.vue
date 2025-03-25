97dba6e34e93077770203de5c68ede3ab6fcc49c
A-n-k-a <47747022+A-n-k-a@users.noreply.github.com>
2025-03-25 23:40:56 +0800

    更改标题：Hi, I'm Anka

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -5,8 +5,10 @@
     <div class="logo">
       <img class="logo-img" :src="siteLogo" alt="logo" />
       <div :class="{ name: true, 'text-hidden': true, long: siteUrl[0].length >= 6 }">
-        <span class="bg">{{ siteUrl[0] }}</span>
-        <span class="sm">.{{ siteUrl[1] }}</span>
+        <!-- <span class="bg">{{ siteUrl[0] }}</span>
+        <span class="sm">.{{ siteUrl[1] }}</span> -->
+        <span class="bg">Hi, </span>
+        <span class="sm">I'm Anka</span>
       </div>
     </div>
     <!-- 简介 -->
